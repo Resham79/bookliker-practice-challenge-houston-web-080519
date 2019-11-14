57dec74d44b0b0ed5f3c058991d6de25d1d896cb
resham <resham.arora@gmail.com>
2019-11-14 12:54:34 -0600

    resham

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {});
+
+fetch('http://localhost:3000/books')
+    .then (function(response){
+        return response.json()
+    })
+    .then (function(books){
+        // console.log(books)
+
+        books.forEach(function(book){
+            const li = document.createElement('li')
+            document.querySelector("#list-panel").append(li)
+            li.append(book.title)
+            li.addEventListener('click', function(e){
+                const img = document.createElement('img')
+                img.setAttribute('src', book.img_url)
+                document.querySelector('#show-panel').append(img)
+                const description = document.createElement('div')
+                description.append(book.description)
+                document.querySelector('#show-panel').append(description)
+                const btn = document.createElement('button')
+                btn.innerText = "Like"
+                document.querySelector('#show-panel').append(btn)
+                    btn.addEventListener("click",)
+            })
+        })
+    })
